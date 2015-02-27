b6a76d113c3acb829ea742bfac86a06aea96343e
Bradley Lowekamp <blowekamp@mail.nih.gov>
2015-02-27 14:48:22 -0500

    Fix project name and update module macro

--- itk-module.cmake
+++ itk-module.cmake
@@ -1,1 +1,22 @@
-itk_module(ITKImageScaleSpace DEPENDS ITKSpatialObjects ITKImageFeature TEST_DEPENDS ITKTestKernel)
+# the top-level README is used for describing this module, just
+# re-used it for documentation here
+get_filename_component( MY_CURENT_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+file( READ "${MY_CURENT_DIR}/README" DOCUMENTATION )
+
+
+# ITK version 4.5 changed it from EXCLUDE_FROM_ALL to EXCLUDE_FROM_DEFAULT
+set( _EXCLUDE "EXCLUDE_FROM_ALL" )
+if (NOT "${ITK_VERSION_MAJOR}.${ITK_VERSION_MINOR}.${ITK_VERSION_MINOR_PATCH}" VERSION_LESS "4.5")
+  set( _EXCLUDE "EXCLUDE_FROM_DEFAULT" )
+endif()
+
+itk_module(ITKScaleSpace
+  DEPENDS
+    ITKSpatialObjects
+    ITKImageFeature
+  TEST_DEPENDS
+    ITKTestKernel
+  DESCRIPTION
+    "${DOCUMENTATION}"
+  ${_EXCLUDE}
+)
